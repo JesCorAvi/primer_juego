8969303716a83523a787075e9f196a5d1f8ecc75
jesCorAvi <jesuscorderoavila1@gmail.com>
2025-10-01 10:58:57 +0200

    casi pero no detecta contacto

--- scenes/main.gd
+++ scenes/main.gd
@@ -6,11 +6,20 @@ var score
 func game_over():
 	$ScoreTimer.stop()
 	$MobTimer.stop()
+	$HUD.show_game_over()
+	$Music.stop()
+	$DeathSound.play()
+
 
 func new_game():
 	score = 0
+	$Music.play()
+	$HUD.update_score(score)
+	$HUD.show_message("Get Ready")
 	$Player.start($StartPosition.position)
 	$StartTimer.start()
+	get_tree().call_group("mobs", "queue_free")
+
 	
 func _on_player_hit() -> void:
 	pass # Replace with function body.
@@ -44,9 +53,15 @@ func _on_mob_timer_timeout():
 
 func _on_score_timer_timeout():
 	score += 1
+	$HUD.update_score(score)
+
 
 func _on_start_timer_timeout():
 	$MobTimer.start()
 	$ScoreTimer.start()
 func _ready():
 	pass
+
+
+func _on_hud_start_game() -> void:
+	pass # Replace with function body.
